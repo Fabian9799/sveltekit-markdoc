02b4fb26e0ca9e651cba2429d15c62a6532cc943
fabian9799 <fabian9799@protonmail.ch>
2023-11-13 03:26:52 +0100

    ✨ added ToC

--- src/markdoc/layouts/Article.svelte
+++ src/markdoc/layouts/Article.svelte
@@ -1,11 +1,27 @@
-<script>
+<script lang="ts">
+	import { generateTableOfContents } from '$lib/toc';
+	import { onMount } from 'svelte';
+
 	export let title = '';
 	export let description = '';
+
+	let article: HTMLDivElement;
+	let toc: HTMLDivElement;
+
+	onMount(() => {
+		const tocElements = generateTableOfContents(article);
+		toc.appendChild(tocElements);
+	});
 </script>
 
 <nav>This is an article</nav>
 <p>Title: {title}</p>
 <p>Description: {description}</p>
-<div class="prose p-4">
+
+<div class="border border-sky-500" bind:this={toc}>
+	<p class="font-bold">Table of contents</p>
+</div>
+
+<div class="prose p-4" bind:this={article}>
 	<slot />
 </div>
