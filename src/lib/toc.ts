08d5be17d0207e0669ddb94fea473e2d528e43f3
fabian9799 <fabian9799@protonmail.ch>
2023-11-14 02:05:11 +0100

    🎨 improved ToC function

--- src/lib/toc.ts
+++ src/lib/toc.ts
@@ -1,20 +1,17 @@
 export function generateTableOfContents(article: HTMLElement) {
 	const headings = article.querySelectorAll('h1, h2, h3, h4, h5, h6');
-	const tocContainer = document.createElement('div');
-	tocContainer.classList.add('table-of-contents');
 	const tocList = document.createElement('ul');
 	headings.forEach((heading, index) => {
 		if (!heading.id) {
 			heading.id = 'toc-heading-' + index;
 		}
 		const tocItem = document.createElement('li');
-
 		const tocLink = document.createElement('a');
 		tocLink.href = '#' + heading.id;
 		tocLink.textContent = heading.textContent;
 		tocItem.appendChild(tocLink);
 		tocList.appendChild(tocItem);
 	});
-	tocContainer.appendChild(tocList);
-	return tocContainer;
+
+	return tocList;
 }
