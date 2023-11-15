00d733584183c46a3d22fccc6e3f3b30c37b57e6
fabian9799 <fabian9799@protonmail.ch>
2023-11-15 17:46:46 +0100

    💄 back button

--- src/markdoc/layouts/Article.svelte
+++ src/markdoc/layouts/Article.svelte
@@ -17,6 +17,21 @@
 <main>
 	<article class="max-w-2xl mx-auto">
 		<header class="my-4">
+			<a class="flex gap-1 w-fit" href="/"
+				><svg
+					xmlns="http://www.w3.org/2000/svg"
+					width="24"
+					height="24"
+					viewBox="0 0 24 24"
+					fill="none"
+					stroke="currentColor"
+					stroke-width="2"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6" /></svg
+				>
+				<span>Back</span>
+			</a>
 			<h1 class="text-5xl font-bold">{title}</h1>
 			<p>{description}</p>
 		</header>
--- src/markdoc/layouts/Author.svelte
+++ src/markdoc/layouts/Author.svelte
@@ -5,6 +5,21 @@
 
 <main class="max-w-2xl mx-auto">
 	<header class="my-4 border p-4 rounded-md shadow">
+		<a class="flex gap-1 w-fit" href="/"
+			><svg
+				xmlns="http://www.w3.org/2000/svg"
+				width="24"
+				height="24"
+				viewBox="0 0 24 24"
+				fill="none"
+				stroke="currentColor"
+				stroke-width="2"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				class="lucide lucide-chevron-left"><path d="m15 18-6-6 6-6" /></svg
+			>
+			<span>Back</span>
+		</a>
 		<div class="flex gap-2">
 			<svg
 				class="h-10 my-auto text-zinc-700"
